6bb1b389c7a4092360ee2175da51079a84c63e93
cornwalljeremy <cornwall.jeremy@gmail.com>
2020-08-16 22:23:32 -0600

    password-generator

--- assets/script.js
+++ assets/script.js
@@ -3,79 +3,79 @@ var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m
 var numericalArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var specialCharacterArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "|", "[", "]", ";", "'", ":", "<", ">", "?", "/", " "];
 
-// Assignment code here
-// 
-
 // Character Length
 function generatePassword() {
-    var passwordLength = window.prompt("How many characters? between 8 and 128");
-    if (passwordLength < 8 || passwordLength > 128) {
-        window.alert("Please enter a value between 8 and 128");
-        lengthPrompt()
-    }
+    var passwordLength = 0
+    do {
+        passwordLength = window.prompt("How many characters? between 8 and 128");
+        if (passwordLength < 8 || passwordLength > 128) {
+            window.alert("Please enter a value between 8 and 128");
+            // generatePassword()
+        }
+    } while (passwordLength < 8 || passwordLength > 128)
     return characterPrompt(passwordLength)
+
 }
 function characterPrompt(passwordLength) {
+
     // Create array here 
     var guaranteedArray = []
     var possibleArray = []
     var resultArray = []
+    //part of the recursive loop issue fix
+    do {
+        // Array of Uppercase
+        var upperCase = confirm("Is Uppercase ok? Yes or No");
+        if (upperCase === true) {
+            guaranteedArray.push(random(upperArray));
+            possibleArray = possibleArray.concat(upperArray)
+        }
+        // Array of lowercase
+        var lowerCase = confirm("Is Lower case ok? Yes or No");
+        if (lowerCase === true) {
+            guaranteedArray.push(random(lowerArray));
+            possibleArray = possibleArray.concat(lowerArray);
+        }
+        //Array of numbers
+        var numerical = confirm(" Are Numbers ok? Yes or No")
+        if (numerical === true) {
+            guaranteedArray.push(random(numericalArray));
+            possibleArray = possibleArray.concat(numericalArray);
+        }
+        //Array of special characters
+        var specialCharacter = confirm("Are Special Characters ok? Yes or No")
+        if (specialCharacter === true) {
+            guaranteedArray.push(random(specialCharacterArray));
+            possibleArray = possibleArray.concat(specialCharacterArray);
+        }
 
-    // Array of Uppercase
-    var upperCase = confirm("Is Uppercase ok? Yes or No");
-    if (upperCase === true) {
-        guaranteedArray.push(random(upperArray));
-        possibleArray = possibleArray.concat(upperArray)
-    }
-    // Array of lowercase
-    var lowerCase = confirm("Is Lower case ok? Yes or No");
-    if (lowerCase === true) {
-        guaranteedArray.push(random(lowerArray));
-        possibleArray = possibleArray.concat(lowerArray);
-    }
-    //Array of numbers
-    var numerical = confirm(" Are Numbers ok? Yes or No")
-    if (numerical === true) {
-        guaranteedArray.push(random(numericalArray));
-        possibleArray = possibleArray.concat(numericalArray);
-    }
-    //Array of special characters
-    var specialCharacter = confirm("Are Special Characters ok? Yes or No")
-    if (specialCharacter === true) {
-        guaranteedArray.push(random(specialCharacterArray));
-        possibleArray = possibleArray.concat(specialCharacterArray);
-    }
-    //it has to have at least one of these
-    if (upperCase === false && lowerCase === false && numerical === false && specialCharacter === false) {
-        window.alert("Please enter at least one value");
-        characterPrompt(passwordLength);
-        // console.log(resultArray)
-    }
+        //it has to have at least one of these
+        if (upperCase === false && lowerCase === false && numerical === false && specialCharacter === false) {
+            window.alert("Please enter at least one value");
+
+        }
+        //this is to get rid of the recursive loop that I had.
+    } while (upperCase === false && lowerCase === false && numerical === false && specialCharacter === false)
+
     for (i = 0; i < passwordLength; i++) {
         resultArray.push(random(possibleArray));
     }
+    //maintain the must include portion challenge
     for (var i = 0; i < guaranteedArray.length; i++) {
         resultArray[i] = guaranteedArray[i];
     }
-    // console.log(resultArray.join(""))
+    //brings it all together
     return resultArray.join("")
 }
-// if (upperCase === false && lowerCase === false && numerical === false && specialCharacter === false) {
-//     window.alert("Please enter at least one value");
-//     characterPrompt(passwordLength);
-// }
+
 //nuts and bolts of the operation
 function random(array) {
     var index = Math.floor(Math.random() * array.length);
     var element = array[index]
     return element;
 }
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
-
 
 // Write password to the #password input
 function writePassword() {
@@ -83,7 +83,6 @@ function writePassword() {
     var passwordText = document.querySelector("#password");
 
     passwordText.value = password;
-
 }
 
 // Add event listener to generate button
